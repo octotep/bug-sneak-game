b66c6740c1043d4662b58e99c0141e5c28615c34
Chris Yealy <christopheryealy@yahoo.com>
2021-11-12 23:26:52 -0500

    Player and animations

--- Camera.gd
+++ Camera.gd
@@ -3,7 +3,7 @@ extends Node2D
 var angle = 0
 var direction = Vector2()
 
-func _process(delta):
+func _process(_delta):
 	var pos = global_position
 	direction = (pos - get_global_mouse_position()).normalized()
 	angle = rad2deg(direction.angle())
