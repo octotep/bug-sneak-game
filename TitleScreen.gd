3b6d26f7d1fbea8810665ebfbce36dc80ce1528b
Daniel Muckerman <danielmuckerman@me.com>
2021-11-13 02:34:28 -0500

    Add pause screen and clean up titlescreen code a bit

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -1,10 +1,13 @@
 extends MarginContainer
 
+func _init():
+	OS.min_window_size = OS.window_size
+	if (OS.get_name() == 'HTML5'):
+		self.visible = false
 
-func _on_Button3_pressed():
+func _on_NewGame_pressed():
+	var _ret = get_tree().change_scene("res://Level.tscn")
+
+func _on_Quit_pressed():
 	get_tree().get_root().notification(NOTIFICATION_WM_QUIT_REQUEST)
 	get_tree().quit()
-
-
-func _on_Button_pressed():
-	var _ret = get_tree().change_scene("res://Level.tscn")
