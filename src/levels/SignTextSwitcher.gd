68fc15a053f6634ab4fd1560b489d1aec42ed98e
Austin Decker <austrideck@gmail.com>
2021-11-30 01:02:45 -0500

    A few level tweaks

--- src/levels/SignTextSwitcher.gd
+++ src/levels/SignTextSwitcher.gd
@@ -5,6 +5,6 @@ func _ready():
 	# We want different text if there's a touch screen,
 	# because touch screens don't have a Z key (usually)
 	if OS.has_touchscreen_ui_hint():
-		$Sign.sign_text += "\n\nTap the zap button to start hacking."
+		$Sign.sign_text += "\n\nTap the zap button to zap."
 	else:
-		$Sign.sign_text += "\n\nPress the 'z' key to start hacking."
+		$Sign.sign_text += "\n\nPress the 'z' key to zap."
