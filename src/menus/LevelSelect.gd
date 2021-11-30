57e5365acc07e9bb29ed5df2da6dffff1aa2b952
Austin Decker <austrideck@gmail.com>
2021-11-30 15:13:04 -0500

    Save and load min alerts per level, display in level select and congrats screen, and also reset save game on starting a new game

--- src/menus/LevelSelect.gd
+++ src/menus/LevelSelect.gd
@@ -1,29 +1,55 @@
 extends MarginContainer
 
 onready var item_list = $VBoxContainer/MarginContainer/HBoxContainer/ItemList
-
-
+onready var levels_unlocked = Global.game_state["levels_unlocked"]
+onready var min_level = int(min(levels_unlocked, len(Global.levels)))
+onready var v_scroll = item_list.get_v_scroll()
+var alert_containers = []
 
 func _ready():
-	var levels_unlocked = Global.game_state["levels_unlocked"]
-	var min_level = int(min(levels_unlocked, len(Global.levels)))
+	
+	# Set this to an arbitrarily high number, since the default is always just 100 for some reason
+	v_scroll.set_max(1000000)
+	v_scroll.set_value(v_scroll.get_max())
+	
 	for i in range(min_level):
 		var level = Global.levels[i]
 		var play_level = load("res://assets/play_level.png")
 		var beat_level = load("res://assets/beat_level.png")
 		if i < levels_unlocked - 1:
 			item_list.add_item(level["text"], beat_level, true)
+			
+			var icon = item_list.get_item_icon(i)
+			var icon_rect = item_list.get_item_icon_region(i)
+			
+			var alert_container = Node2D.new()
+			alert_container.position.x = icon_rect.position.x
+			var y_offset = icon.get_height() + get_constant("icon_margin", "ItemList")
+			y_offset *= i
+			alert_container.position.y = y_offset + 6 - v_scroll.get_value()
+			
+			var alert_label = item_list.get_node("AlertLabel").duplicate()
+			alert_label.text = "ALERTS\n" + str(Global.game_state["alert_counts"][i])
+			
+			alert_container.add_child(alert_label)
+			item_list.add_child(alert_container)
+			alert_containers.append(alert_container)
 		else:
-			item_list.add_item(level["text"], play_level, true)			
-	
-	var v_scroll = item_list.get_v_scroll()
-	v_scroll.set_value(v_scroll.get_max())
+			item_list.add_item(level["text"], play_level, true)
 	
 	Global.save_game()
 	
 	var player = get_node("/root/MusicPlayer")
 	player.play_titlescreen_bgm()
-	
+
+func _process(delta):
+	for i in range(alert_containers.size()):
+		var container = alert_containers[i]
+		var icon = item_list.get_item_icon(i)
+		var y_offset = icon.get_height() + get_constant("icon_margin", "ItemList")
+		y_offset *= i
+		container.position.y = y_offset + 6 - v_scroll.get_value()
+
 func _exit_tree():
 	var player = get_node("/root/MusicPlayer")
 	player.play_level_bgm()
--- src/singletons/global.gd
+++ src/singletons/global.gd
@@ -1,8 +1,11 @@
 extends Node
 
-var game_state = {
-	"levels_unlocked": 0
+var base_game_state = {
+	"levels_unlocked": 0,
+	"alert_counts": []
 }
+
+var game_state = base_game_state
 
 var levels = [
 	{
@@ -25,9 +28,22 @@ var levels = [
 
 var current_level = -1
 
+# Some light validation to make sure we're not saving bad data
+func validate_game_state():
+	
+	# Make sure we have the right number of alert counts
+	var alert_counts = game_state["alert_counts"]
+	if alert_counts.size() > levels.size():
+		alert_counts.resize(levels.size())
+	while alert_counts.size() < levels.size():
+		# We're not using resize here because that populates with nulls, we want -1 instead
+		alert_counts.append(-1)
+
 func save_game():
 	var save_file = File.new()
 	save_file.open("user://bug_sneak_game.save", File.WRITE)
+	
+	validate_game_state()
 	
 	save_file.store_line(to_json(game_state))
 
@@ -44,11 +60,20 @@ func load_game():
 	game_state = parse_json(save_file.get_line())
 
 # If we beat the current level, unlock the next one (level select screen prevents overflows)
-func beat_level():
+func beat_level(alert_count):
 	if (current_level + 1) == game_state["levels_unlocked"]:
 		game_state["levels_unlocked"] += 1
-	save_game()
 	
+	# Store the number of alerts, if it's lower than the current record
+	var current_record = game_state["alert_counts"][current_level]
+	if current_record == -1 or alert_count < current_record:
+		game_state["alert_counts"][current_level] = alert_count
+	
+	save_game()
+
+func get_alert_record(level):
+	var record = -1
+
 func did_win_game():
 	if (current_level + 1) == len(levels):
 		return true
